af1cf794cc87f09b91f2e757885150b2b958156f
craigxiang <31474900+craigxiang@users.noreply.github.com>
2019-02-16 15:55:54 +0800

    Seventh Submit
    JavaSE上课+练习

--- JavaSETest/src/com/test1/test1.java
+++ JavaSETest/src/com/test1/test1.java
@@ -1,33 +1,82 @@
 package com.test1;
 
-//遍历数组，排序
 public class test1 {
     public static void main(String[] args) {
+        //遍历数组，冒泡排序，二分查找
         int[] arr;
-        arr=new int[] {2,5,1,62,3,55,4};
+        arr=new int[]{9,8,3,7,5,2,89,37,27,15,21,42,69,57,4};
 
-        System.out.print("数组中的数值是：");
+        System.out.print("（排序前）数组：");
+        printArray(arr);
 
-        for(int i=0;i<arr.length;i++) {
-            System.out.print(" "+arr[i]+" ");
-        }
+        BubbleSort(arr);
 
-        System.out.print("\n");
-        //排序
-        int max;
-        max=getMax(arr);
-        System.out.print("数组中最大的值是："+max);
+        System.out.print("(排序后)数组：");
+        printArray(arr);
 
-    }
+        //使用二分查找法查找指定数字
+        int myNum=89;
 
-    public static int getMax(int[] arr) {
-        int max;
-        max=arr[0];
-        for(int i=0;i<arr.length;i++) {
-            if(arr[i]>max) {
-                max=arr[i];
+        int start=0;
+        int end=arr.length-1;
+
+
+        while(true) {
+            int middle=(start+end)/2;
+            if(myNum>arr[middle]) {
+                start=middle+1;
+            }else if(myNum<arr[middle]) {
+                end=middle-1;
+            }else {
+                System.out.print("找到此数"+myNum+"  位置是："+(middle+1));
+                break;
+            }
+            if(start>end) {
+                System.out.print("查无此数");
+                break;
+
             }
         }
-        return max;
+
+    }
+    //定义一个打印数组的方法
+    public static void printArray(int[] arr) {
+        for(int i=0;i<arr.length;i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.print("\n");
+    }
+
+    //定义一个冒泡排序的方法
+    public static void BubbleSort(int[] arr) {
+        for(int i=0;i<arr.length-1;i++) {
+            for(int j=0;j<arr.length-i-1;j++) {
+                int temp;
+                if(arr[j]>arr[j+1]) {
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+            System.out.print("第"+i+"轮排序后的结果为：");
+            printArray(arr);
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
